488bfa2f27dcfd03be6bbf740076e40cf4c57e0f
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-10-13 18:23:26 +0600

    ui update

--- resources/js/admin/Pages/profile.vue
+++ resources/js/admin/Pages/profile.vue
@@ -1,3 +1,12 @@
 <template>
-    <div class="">Profile</div>
+ <div class="profile-page">
+     <div class="page-header my-5"></div>
+     <div class="container">
+         <div class="card rounded-4 shadow p-3 mb-3 border-0">
+             <div class="card-header bg-white">
+                 <h3 class="card-title">Profile</h3>
+             </div>
+         </div>
+     </div>
+ </div>
 </template>
