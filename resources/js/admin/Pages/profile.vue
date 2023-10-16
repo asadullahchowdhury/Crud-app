a9e15d0d28a5fb7aed77c5ad56c063215656795c
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-10-16 18:50:28 +0600

    profile page update

--- resources/js/admin/Pages/profile.vue
+++ resources/js/admin/Pages/profile.vue
@@ -3,10 +3,10 @@
      <div class="page-header my-5"></div>
      <div class="container">
          <div class="card rounded-4 shadow p-3 mb-3 border-0">
-             <div class="card-header bg-white">
+             <div class="card-header d-flex justify-content-between align-items-center bg-white">
                  <h3 class="card-title">Profile</h3>
+                 <a href="javascript:void(0)" class="btn btn-light">Edit</a>
              </div>
-             .
          </div>
      </div>
  </div>
